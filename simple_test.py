7fa8ff47e2f564bf885e170fec421b9ca80a9b08
bkogler <bernhard.kogler@gmail.com>
2022-12-17 22:31:37 +0100

    Added functions to simplify charge management

--- simple_test.py
+++ simple_test.py
@@ -3,9 +3,20 @@ import json
 from goecharger.goecharger import GoeCharger
 
 charger = GoeCharger("goecharger.lan")
-charger.set_forced_state(charger.SettableValueEnums.ForcedState.neutral)
 
 status = charger.get_status(status_type=charger.STATUS_MINIMUM)
-
 status = charger.get_status(("fna", "oem"))
+
+status = charger.get_status(status_type=charger.STATUS_DEFAULT)
+print(json.dumps(status, sort_keys=True, indent=4))
+
+charger.set_phase_mode(charger.SettableValueEnum.PhaseMode.three)
+charger.set_ampere(16)
+
+charger.set_charging_mode(charger.SettableValueEnum.ChargingMode.neutral)
+
+status = charger.get_ampere()
+print(json.dumps(status, sort_keys=True, indent=4))
+
+status = charger.get_phase_mode()
 print(json.dumps(status, sort_keys=True, indent=4))
